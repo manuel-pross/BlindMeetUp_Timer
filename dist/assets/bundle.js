c51221e653654f8c5b51ac0781ad501814d905d8
manuel-pross <48802396+manuel-pross@users.noreply.github.com>
2020-11-01 11:59:38 +0100

    TASK: add redesign

--- dist/assets/bundle.js
+++ dist/assets/bundle.js
@@ -12,12 +12,10 @@ function updateTimer() {
         const days = Math.floor(distance / (1000 * 60 * 60 * 24));
         const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
         document.querySelector("#days").innerHTML = days;
         document.querySelector("#hours").innerHTML = hours;
         document.querySelector("#minutes").innerHTML = minutes;
-        document.querySelector("#seconds").innerHTML = seconds;
     }, 1000)
 }
 
